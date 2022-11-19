68ed9a78ffcd3d63329dede40b7585b7b6908359
Victor Ionescu <victor@meldeenvermoeden.nl>
2022-11-19 19:24:55 +0100

    add alternate screen

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,20 @@ use std::io;
 
 mod file_select;
 use crate::file_select::select_file_path;
+
 mod parse_file;
 use crate::parse_file::parse_file;
 
+mod alternate_screen;
+use crate::alternate_screen::alt_screen;
+
 fn main() -> Result<(), io::Error> {
-    let file_path = select_file_path();
+    alt_screen()?;
+    // let file_path = select_file_path();
 
-    println!("Selected file: {}", file_path);
+    // println!("Selected file: {}", file_path);
 
-    let subtitles = parse_file(file_path);
+    // let subtitles = parse_file(file_path);
 
     Ok(())
 }
